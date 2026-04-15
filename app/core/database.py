a5b54fe20214373f4bd8ce28bc32fa1ccd6b5886
vadalasathwik <vadalasathwik@gmail.com>
2026-04-15 21:05:43 +0530

    azure deployment setup

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,14 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("postgresql://resume_analyzer_db:hLBOQB5DaqaI4b4Ca9YlSwXXNWNpr4Hr@dpg-d7f3hv57vvec73chtnlg-a/resume_analyzer_db_siihL")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
